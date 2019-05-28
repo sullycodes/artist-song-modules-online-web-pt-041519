aa6964364d4b2e2c3ad9559383125798ac6786aa
Daniel Sullivan <dansullivan19@gmail.com>
2019-05-28 16:40:59 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,10 +2,8 @@ require 'pry'
 
 class Artist
   
-  extend
-  Memorable::ClassMethods
-  include
-  Memorable::InstanceMethods
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
   extend Findable
   include Paramable
   
